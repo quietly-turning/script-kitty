4e4add9db3d82bd128b3b8aabb08752744d79f72
Dan Guzek <dguzek@gmail.com>
2014-07-02 13:57:02 -0400

    exercise and query migrations support answers

--- db/migrate/20140628205401_create_queries.rb
+++ db/migrate/20140628205401_create_queries.rb
@@ -4,6 +4,8 @@ class CreateQueries < ActiveRecord::Migration
       t.integer :dummy_id
       t.text :formatted_sql
       t.text :raw_sql
+	  t.text :sorted_sql
+	  t.boolean :correct, :default => false
       t.text :html_table, :limit => 4294967295
       t.references :user, index: true
       t.references :exercise, index: true
--- db/migrate/20140630051525_create_exercises.rb
+++ db/migrate/20140630051525_create_exercises.rb
@@ -2,7 +2,10 @@ class CreateExercises < ActiveRecord::Migration
   def change
     create_table :exercises do |t|
       t.text :question
-
+	  t.text :answer
+	  t.text :response_correct
+	  t.text :response_incorrect
+	  
       t.timestamps
     end
   end
