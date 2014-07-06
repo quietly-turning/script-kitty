caaac6b8079f1dd5e7deaf5a2e6caefeb9a592ae
Dan Guzek <dguzek@gmail.com>
2014-07-06 01:06:21 -0400

    add fields to Exercise table
    "Description" and "Lesson"

--- db/migrate/20140630051525_create_exercises.rb
+++ db/migrate/20140630051525_create_exercises.rb
@@ -3,6 +3,8 @@ class CreateExercises < ActiveRecord::Migration
     create_table :exercises do |t|
       t.text :question
 	  t.text :answer
+	  t.text :description
+	  t.text :lesson
 	  t.text :response_correct
 	  t.text :response_incorrect
 	  
