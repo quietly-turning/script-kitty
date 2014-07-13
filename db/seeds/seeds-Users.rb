d1cde4c01e6bb5f979e27bbacc93464bd467d9cd
Dan Guzek <dguzek@gmail.com>
2014-07-13 17:30:43 -0400

    update seed file for Operators

--- db/seeds/seeds-Users.rb
+++ db/seeds/seeds-Users.rb
@@ -8,11 +8,11 @@ User.create(admin: 1, visual_interface: 0, email: 'test@test.com', password: 'to
 #          Seed the Operators
 ####################################
 
-Operator.create( name: 'equals',        sql_value: '=',       html_representation: '=')
-Operator.create( name: 'like',          sql_value: ' like ',  html_representation: 'has a')
-Operator.create( name: 'greaterthan',   sql_value: '>',       html_representation: '&gt;')
-Operator.create( name: 'lessthan',      sql_value: '<',       html_representation: '&lt;')
-Operator.create( name: 'doesnotequal',  sql_value: '<>',      html_representation: '&ne;')
+Operator.create( name: 'equals',        sql_value: ' = ',		html_representation: '=')
+Operator.create( name: 'like',          sql_value: ' like ',	html_representation: 'contains')
+Operator.create( name: 'greaterthan',   sql_value: ' > ',		html_representation: '&gt;')
+Operator.create( name: 'lessthan',      sql_value: ' < ',		html_representation: '&lt;')
+Operator.create( name: 'doesnotequal',  sql_value: ' != ',		html_representation: '&ne;')
 
 
 ####################################
