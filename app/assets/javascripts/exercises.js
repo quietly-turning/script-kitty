1f74ea616b7c5c79112ef930c53344ff0a8879ee
Dan Guzek <dguzek@gmail.com>
2014-10-27 16:18:07 -0400

    cleanup simple query review

--- app/assets/javascripts/exercises.js
+++ app/assets/javascripts/exercises.js
@@ -1,17 +1,29 @@
 $(function(){
 	
-	var myTextArea = document.getElementById("query_raw_sql");
+	var simple_editor = document.getElementById("query_raw_sql");
+	var raw_sql = $("#raw-sql")[0];
 	
 	// ensure that we found a textarea
 	// (participants using the visual builder will not)
-	if (myTextArea != null)
+	if (simple_editor != null)
 	{
-		var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
+		var myCodeMirror = CodeMirror.fromTextArea(simple_editor, {
 			mode: "text/x-mariadb",
 			lineNumbers: true,
 			theme: "lesser-dark"
 		});	
 	}
+	
+	if (raw_sql != null)
+	{
+		var myCodeMirror = CodeMirror.fromTextArea(raw_sql, {
+			mode: "text/x-mariadb",
+			lineNumbers: true,
+			readOnly: "nocursor",
+		});
+	}
+	
+	
 	
 	$('#exercise-footer').height(function(index, height) {
 	    return window.innerHeight - $(this).offset().top;
