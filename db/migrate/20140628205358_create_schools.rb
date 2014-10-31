51b2a75f4dc937109b828da79398e5650e7f18ce
Dan Guzek <dguzek@gmail.com>
2014-10-30 22:00:29 -0400

    prune schools migration

--- db/migrate/20140628205358_create_schools.rb
+++ db/migrate/20140628205358_create_schools.rb
@@ -3,8 +3,8 @@ class CreateSchools < ActiveRecord::Migration
     create_table :schools do |t|
       t.string :name
       t.string :city
-      t.string :state
-      t.string :zip
+      t.string :state, :limit => 2
+      t.string :zip, :limit => 10
       t.string :chief
       t.references :control, index: true
       t.references :locale, index: true
