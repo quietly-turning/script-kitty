71cb69f4b31f9fb1fee424e75a31ff35b88edf32
Dan Guzek <dguzek@gmail.com>
2014-10-31 01:15:15 -0400

    fix query migration
    It seems that MySQL does not allow text columns to have a default value. See: http://stackoverflow.com/questions/10747997/how-to-change-a-text-column-that-default-to-null-to-default-to-in-a-rails-mig

--- db/migrate/20140628205401_create_queries.rb
+++ db/migrate/20140628205401_create_queries.rb
@@ -1,12 +1,11 @@
 class CreateQueries < ActiveRecord::Migration
   def change
     create_table :queries do |t|
-      t.integer :dummy_id
       t.text :raw_sql
 	  t.integer :status, :limit => 1, :default => 0
 	  t.integer :truncated_results, :limit => 1, :default => 0
 	  t.integer :result_size, :limit => 2
-      t.text :html_table, :limit => 4294967295, :default => "<em>( no results returned )</em>"
+      t.text :html_table, :limit => 4294967295
       t.references :user, index: true
       t.references :exercise, index: true
 
