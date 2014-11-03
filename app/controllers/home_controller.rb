f038ac138261fa2c904a860bfcb41ffb68d6a6b3
Dan Guzek <dguzek@gmail.com>
2014-11-02 21:53:09 -0500

    create a placeholder "About" page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
-  def index
-      respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @query }
-    end
-  end
+	def index
+		respond_to do |format|
+			format.html # new.html.erb
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   
   root :to => "home#index"
+  get ':action' => 'home#:about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
