d6ff63dcc3cc001b905da617c66b7259effb6ad5
Dan Guzek <dguzek@gmail.com>
2014-11-17 11:08:15 -0500

    remove deprecated exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,11 @@ class ApplicationController < ActionController::Base
 
 	layout "application"
 
-    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
     private
-      def record_not_found
-        redirect_to root_path and return
-      end
 
-	  def verify_is_admin
-	    (not current_user) ? (redirect_to root_path and return) : (redirect_to root_path and return unless current_user.admin?)
-	  end
+		def verify_is_admin
+	    	(not current_user) ? (redirect_to root_path and return) : (redirect_to root_path and return unless current_user.admin?)
+		end
 
 	  def after_sign_in_path_for(resource)
   		if current_user and Query.where(user_id: current_user.id).size > 0
