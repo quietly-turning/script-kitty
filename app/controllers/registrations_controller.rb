4ab0f562b5a777af0eda4d698460e5502f3b4194
Dan Guzek <dguzek@gmail.com>
2015-03-03 20:27:40 -0500

    misc cleanup for new admin pages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
- 
+
     # def create
     #     super do |resource|
     #         resource.visual_interface = [true, false].sample
     #     end
     # end
-    
+
 end
