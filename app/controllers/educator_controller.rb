1fdebb199be0f13fffbf71ce3356a16102b3c410
Dan Guzek <dguzek@gmail.com>
2015-03-06 13:39:43 -0500

    abstract chart-building logic out to controller

--- app/controllers/educator_controller.rb
+++ app/controllers/educator_controller.rb
@@ -12,15 +12,47 @@ class EducatorController < ApplicationController
 
 	def exercises
 		@lessons = Lesson.all
-		@exercises = Exercise.order(:id).page params[:page]
+		@unified_table_data = [["Lesson", "Correct Attempts", "Valid Attempts", "Invalid Attempts"]]
+		@lessons.each do |lesson|
+			@unified_table_data << [
+				lesson.id.to_s,
+				Lesson.joins(exercises: :queries).where(id: lesson.id, queries: {status: 2}).count,
+				Lesson.joins(exercises: :queries).where(id: lesson.id, queries: {status: 1}).count,
+				Lesson.joins(exercises: :queries).where(id: lesson.id, queries: {status: 0}).count
+			]
+		end
 
-		@columns = 	[
-						{type: "number", name:  "Total Attempts" },
-						{type: "number", name:  "Correct Attempts" },
-			 			{type: "number", name:  "Valid Attempts" },
-						{type: "number", name:  "Invalid Attempts" },
-						{type: "number", name:  "Unique Learners" },
-					]
+
+		@exercises = Exercise.order(:id).page params[:page]
+		@columns = ["Invalid Attempts", "Valid Attempts", "Correct Attempts",  "Total Attempts" , "Unique Learners"]
+		@table_data = {}
+
+		@columns.each_with_index do |column, i|
+
+			@table_data[column] = []
+			@table_data[column] << ['Exercise ID', column]
+
+			@exercises.each do |exercise|
+
+				@table_data[column] << [
+					# exercise id
+					exercise.lesson_id.to_s +  "." + exercise.dummy_id.to_s,
+
+					# total attempts column
+					if i == 3
+						exercise.queries.size
+
+					# unique learners column
+					elsif i == 4
+						exercise.queries.select(:user_id).distinct.count
+
+					# [invalid, valid, correct] attempts column
+					elsif i >= 0 and i < 3
+						exercise.queries.where(status: i).size
+					end
+				]
+			end
+		end
 
 	end
 
