a0ef8f27d0f1b79db15071564f309f3e3029069c
Dan Guzek <dguzek@gmail.com>
2015-03-10 17:35:58 -0400

    make Educator/Exercises datatable sortable
    Sortable tables for learners weaken the value of learning querying, but sortable tables for the educators are helpful!

--- app/controllers/educator_controller.rb
+++ app/controllers/educator_controller.rb
@@ -12,19 +12,6 @@ class EducatorController < ApplicationController
 
 	def exercises
 		@lessons = Lesson.all
-		@master_table_data = [["Lesson", "Correct Attempts", "Valid Attempts", "Invalid Attempts"]]
-
-		@lessons.each do |lesson|
-			@master_table_data << [
-				lesson.id.to_s,
-				Lesson.joins(exercises: :queries).where(id: lesson.id, queries: {status: 2}).count,
-				Lesson.joins(exercises: :queries).where(id: lesson.id, queries: {status: 1}).count,
-				Lesson.joins(exercises: :queries).where(id: lesson.id, queries: {status: 0}).count
-			]
-		end
-
-
-		# --------------------------------------------------------------------------------
 		@exercises = Exercise.order(:id).page params[:page]
 		@columns = ["Correct Attempts", "Valid Attempts", "Invalid Attempts",  "Total Attempts" , "Unique Learners"]
 		@colors = {
@@ -34,16 +21,50 @@ class EducatorController < ApplicationController
 			"Total Attempts" 	=> "#ec814d",
 			"Unique Learners" 	=> "#9881f5"
 		}
-		@table_data = {}
+
+		# --------------------------------------------------------------------------------
+		@master_chart_data = [["Lesson", "Correct Attempts", "Valid Attempts", "Invalid Attempts"]]
+
+		@lessons.each do |lesson|
+			@master_chart_data << [
+				lesson.id.to_s,
+				Lesson.joins(exercises: :queries).where(id: lesson.id, queries: {status: 2}).count,
+				Lesson.joins(exercises: :queries).where(id: lesson.id, queries: {status: 1}).count,
+				Lesson.joins(exercises: :queries).where(id: lesson.id, queries: {status: 0}).count
+			]
+		end
+
+
+		# --------------------------------------------------------------------------------
+		@chart_data = {}
+		@table_data = [["ID","Total Attempts", "Correct", "Valid", "Invalid", "Unique Learners"]]
+
+		@exercises.each do |exercise|
+			@table_data << [
+				# exercise id
+				exercise.lesson_id.to_s +  "." + exercise.dummy_id.to_s,
+				# total
+				exercise.queries.size,
+				# correct
+				exercise.queries.where(status: 2).size,
+				# valid
+				exercise.queries.where(status: 1).size,
+				# invalid
+				exercise.queries.where(status: 0).size,
+				# unique learners
+				exercise.queries.select(:user_id).distinct.count
+			]
+		end
+
 
 		@columns.each do |column|
 
-			@table_data[column] = []
-			@table_data[column] << ['Exercise ID', column]
+			@chart_data[column] = []
+			@chart_data[column] << ['Exercise ID', column]
 
 			@exercises.each do |exercise|
 
-				@table_data[column] << [
+				@chart_data[column] << [
 					# exercise id
 					exercise.lesson_id.to_s +  "." + exercise.dummy_id.to_s,
 
